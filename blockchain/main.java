f13d2210f48d6c27876a212cab8122a3e6b34d54
DLC <55656405+dlc-01@users.noreply.github.com>
2023-03-17 20:44:16 +0300

    Update main.java

--- blockchain/main.java
+++ blockchain/main.java
@@ -1,15 +1,18 @@
 package blockchain;
+import java.security.MessageDigest;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Scanner;
+
+
 public class Main {
     public static void main(String[] args) {
-
-        BlockChain blockchain = new BlockChain();
-        
-        for (int i = 0; i < 4; i++) {
-            blockchain.addBlockChain();
-        }
-        blockchain.printBlockchain();
+        System.out.print("Enter how many zeros the hash must start with: ");
+        final var zeros = new Scanner(System.in).nextInt();
+        final var blockchain = BlockChain.getInstance(zeros);
+        blockchain.generateBlocks(5);
+        System.out.println(blockchain);
 
 
     }
 }
-
